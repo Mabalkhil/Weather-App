7e5bcd6e1047813cf1632d676bdd3a1a7ba600f6
Mohammed Abalkhil <m.abalkhil@outlook.com>
2017-07-11 13:09:02 +0300

    Initial Commit

---
+++ RainyShinyCloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainyShinyCloudy
+//
+//  Created by Mohammed Abalkhail on 7/11/17.
+//  Copyright © 2017 Mohammed Abalkhail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
